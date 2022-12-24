18d0525476d9769f08e120742adb060233b32546
milessteamaccount <ronaldlamdev@gmail.com>
2022-12-23 20:47:58 -0600

    reset code to former state before adding filter

--- src/assets/components/JobCard.tsx
+++ src/assets/components/JobCard.tsx
@@ -1,16 +1,16 @@
 import { ReactElement } from 'react';
 
 interface JobsProp  {
-  company: ReactElement,
-  logo: ReactElement,
+  company: string,
+  logo: string,
   newJob?: ReactElement,
   featured?: ReactElement,
-  position: ReactElement,
-  role: ReactElement,
-  level: ReactElement,
-  postedAt: ReactElement,
-  contract: ReactElement,
-  location: ReactElement,
+  position: string,
+  role: string,
+  level: string,
+  postedAt: string,
+  contract: string,
+  location: string,
   languages: string[],
   tools: string[]
 }
@@ -20,17 +20,17 @@ const JobCard = ({company, logo, newJob, featured, position, role, level, posted
     
     <div className="relative mb-16 flex flex-col md:flex-row md:items-center gap-4 md:gap-20 lg:gap-4 justify-between rounded-md px-4 py-8 border-l-[#5ba4a4] border-l-4 bg-white shadow-xl shadow-[#5ba4a4]/30">
       <div className='md:flex items-center gap-4'>
-        {logo}
+        <img src={logo} alt={company} className='absolute md:static top-[-2rem] w-[3.75rem] h-[3.75rem] md:w-[5.1rem] md:h-[5.1rem]' />
         <div className="text-left text-lg flex flex-col gap-2">
-          <div className='flex'>{company} {newJob} {featured}</div>
-          {position}
-          <div className='flex items-center gap-2'>{postedAt} <div className='w-2 h-2 rounded-full bg-[#7b8e8e]/70' /> {contract} <div className='w-2 h-2 rounded-full bg-[#7b8e8e]/70' /> {location}</div>
+          <div className='flex'><h1 className='text-[#5ba4a4] font-bold text-xl'>{company}</h1> {newJob} {featured}</div>
+          <h1 className='text-xl font-bold'>{position}</h1>
+          <div className='flex text-[#7b8e8e] items-center gap-2'>{postedAt} <div className='w-2 h-2 rounded-full bg-[#7b8e8e]/70' /> {contract} <div className='w-2 h-2 rounded-full bg-[#7b8e8e]/70' /> {location}</div>
         </div>
       </div>
       <div className='w-full h-[.125rem] bg-[#7b8e8e]/50 md:hidden' />
       <div className='flex gap-4 flex-wrap font-bold text-lg'>
-        {role}
-        {level}
+      <button className='p-2 bg-[#effafa] text-[#5ba4a4] rounded cursor-pointer hover:bg-[#5ba4a4] hover:text-white duration-300'>{role}</button>
+      <button className='p-2 bg-[#effafa] text-[#5ba4a4] rounded cursor-pointer hover:bg-[#5ba4a4] hover:text-white duration-300'>{level}</button>
         {languages}
         {tools}
       </div>
--- src/assets/sections/Listings.tsx
+++ src/assets/sections/Listings.tsx
@@ -1,52 +1,7 @@
 import JobCard from "../components/JobCard";
 import { list } from "../data/data";
-import {useState, useEffect} from 'react';
 
 const Listings = () => {
-
-  const [jobs, setJobs] = useState<any[]>([]);
-  const [filters, setFilters] = useState<any[]>([]);
-  const [filteredJobs, setFilteredJobs] = useState<any[]>([]);
-
-  useEffect(() => {
-    setJobs(list);
-  }, []);
-
-  useEffect(() => {
-    setFilteredJobs(jobs);
-  }, [jobs]);
-
-  useEffect(() => {
-    doFilter();
-  }, [filters]);
-
-  // Filter functions
-
-  const addFilter = (el:string) => {
-    !filters.includes(el) && setFilters(filters => [...filters, el]);
-  };
-
-  const removeFilter = (el:string) => {
-    setFilters(filters => filters.filter((elem:string) => {
-      return elem != el;
-    }));
-  };
-
-  const doFilter = () => {
-    setFilters(
-      jobs.filter((item) => {
-        filters.every((filter) => {
-          [item.role, item.level, ...item.languages, ...item.tools].includes(filter)
-        });
-      })
-    );
-  };
-
-  // Reset filters
-
-  const clear = () => {
-    setFilters([]);
-  }
 
   return (
     <main className="px-4 lg:px-24 my-8 bg-[#effafa]">
@@ -54,43 +9,39 @@ const Listings = () => {
       {/* Filter Component */}
       <div className="bg-white px-4 py-8 mb-20 shadow-xl shadow-[#5ba4a4]/30 border-l-4 rounded-md border-[#5ba4a4] flex justify-between">
         <div>
-          {filters.map((el) => {
-            return (
-              <div>{el}<span onClick={() => {removeFilter(el)}}>X</span></div>
-            )
-          })}
+          
         </div>
-        <span onClick={clear} className="text-[#5ba4a4] text-lg font-bold hover:underline hover:underline-offset-2 duration-500 cursor-pointer">Clear</span>
+        <span className="text-[#5ba4a4] text-lg font-bold hover:underline hover:underline-offset-2 duration-500 cursor-pointer">Clear</span>
       </div>
 
-      {/* Jobs mapped out */}
-      {filteredJobs.map((job, i) =>  {
+      {/* Map out from lists */}
+      {list.map((job) => {
         return (
-          <JobCard 
-            key={i}
-            company={<h1 className='text-[#5ba4a4] font-bold text-xl'>{job.company}</h1>}
-            logo={<img className='absolute md:static top-[-1.8rem] w-[3.25rem] md:w-[5rem] h-[3.25rem] md:h-[5rem]' src={job.logo}/>}
-            position={<span className='font-bold w-fit text-lg cursor-pointer hover:text-[#5ba4a4] duration-300'>{job.position}</span>}
+          <JobCard
+            company={job.company}
+            logo={job.logo}
             newJob={(job.new === true) ? <span className="uppercase text-white ml-4 text-base bg-[#5ba4a4] py-1 px-2 rounded-3xl">new!</span> : undefined}
-            featured={(job.featured === true) ? <span className="uppercase text-white ml-2 text-base bg-[#2c3a3a] px-2 py-1 rounded-3xl">featured</span> : undefined}
-            role={<button className='p-2 bg-[#effafa] text-[#5ba4a4] rounded cursor-pointer hover:bg-[#5ba4a4] hover:text-white duration-300'>{job.role}</button>}
-            level={<button className='p-2 bg-[#effafa] text-[#5ba4a4] rounded cursor-pointer hover:bg-[#5ba4a4] hover:text-white duration-300'>{job.level}</button>}
-            postedAt={<span className="text-[#7b8e8e]">{job.postedAt}</span>}
-            contract={<span className="text-[#7b8e8e]">{job.contract}</span>}
-            location={<span className="text-[#7b8e8e]">{job.location}</span>}
+            featured={(job.new === true) ? <span className="uppercase text-white ml-4 text-base bg-[#2c3a3a] py-1 px-2 rounded-3xl">featured!</span> : undefined}
+            position={job.position}
+            role={job.role}
+            level={job.level}
+            postedAt={job.postedAt}
+            contract={job.contract}
+            location={job.location}
             languages={job.languages.map((l:string, i:number) => {
               return (
-                <button onClick={(l) => addFilter(l)} className="p-2 bg-[#effafa] text-[#5ba4a4] rounded cursor-pointer hover:bg-[#5ba4a4] hover:text-white duration-300" key={i}>{l}</button>
+                <button key={i} className="p-2 bg-[#effafa] text-[#5ba4a4] rounded cursor-pointer hover:bg-[#5ba4a4] hover:text-white duration-300">{l}</button>
               )
             })}
             tools={job.tools.map((t:string, i:number) => {
               return (
-                <button onClick={(t) => addFilter(t)} className="p-2 bg-[#effafa] text-[#5ba4a4] rounded cursor-pointer hover:bg-[#5ba4a4] hover:text-white duration-300" key={i}>{t}</button>
+                <button key={i} className="p-2 bg-[#effafa] text-[#5ba4a4] rounded cursor-pointer hover:bg-[#5ba4a4] hover:text-white duration-300">{t}</button>
               )
             })}
           />
         )
       })}
+
     </main>
   )
 }
